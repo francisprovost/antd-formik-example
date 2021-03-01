9b6bb2ba103c427c90cbc52c2f5c8f5504549d57
Francis Provost <me@francisprovost.com>
2021-03-01 12:01:18 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "antd";
+import "antd/dist/antd.css";
+import { Formik } from "formik";
+import "./styles.css";
+
+// TODO Components to have
+// - text input
+// - radio
+// - Select
+// - Lists
+
+export default function App() {
+  return (
+    <div className="App">
+      <Formik></Formik>
+    </div>
+  );
+}
